e0d1bf3856a178cbd6c2dff75e704c0b344a5c38
paloknapo <palo.knapo@gmail.com>
2023-08-24 16:08:39 +0200

    Update make_comment.py

--- make_comment.py
+++ make_comment.py
@@ -52,7 +52,7 @@ def make_comment(
                         "camera": {
                             "isOrthoProjection": False,
                             "position": [
-                                -14.959975903859306,
+                                -15.959975903859306,
                                 109.21340462426888,
                                 19.00868018548827,
                             ],
